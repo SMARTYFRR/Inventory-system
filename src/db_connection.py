3ef16fc41bbbcfc6c80c1a50abce902efbbfc20a
Anshu <165649234+SMARTYFRR@users.noreply.github.com>
2025-12-22 00:44:01 +0530

    Update database connection password placeholder

--- src/db_connection.py
+++ src/db_connection.py
@@ -4,6 +4,6 @@ def get_connection():
     return mysql.connector.connect(
         host="localhost",
         user="root",
-        password="Cr7isdagoat",   #put yo my SQL pass here ngga
+        password="your_real_password",   #put yo my SQL pass here ngga
         database="inventory_db"
     )
